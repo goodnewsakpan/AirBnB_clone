6e1e2242642b7eeda8227c95345d272026bd4969
goodnews akpan <goodnewsakpan123@gmail.com>
2024-03-10 22:30:47 +0100

    A class to show the user information

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 from models.base_model import BaseModel
+"""Base Model"""
 
 
 class User(BaseModel):
+	"""A class rperesenting the user information"""
 	first_name = ""
 	last_name = ""
 	email = ""
