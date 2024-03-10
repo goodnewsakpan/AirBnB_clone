edb1a37119a6567fc3c04bca3984a2b86a92695a
Kenechukwu Akubue <kengoon19@gmail.com>
2024-03-10 18:22:39 +0100

    Implement a global class call

---
+++ libs/__init__.py
@@ -0,0 +1,22 @@
+classes = {}
+
+
+def get_classes():
+    from models.base_model import BaseModel
+    from models.user import User
+    from models.city import City
+    from models.place import Place
+    from models.amenity import Amenity
+    from models.state import State
+    from models.review import Review
+
+    global classes
+    classes |= {
+        "BaseModel": BaseModel,
+        "User": User,
+        "City": City,
+        "Place": Place,
+        "Amenity": Amenity,
+        "State": State,
+        "Review": Review
+    }
