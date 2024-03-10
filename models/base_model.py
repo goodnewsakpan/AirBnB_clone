d390ec96de1121cea3d122fbae922a44a7b8ac1c
goodnews akpan <goodnewsakpan123@gmail.com>
2024-03-10 22:50:18 +0100

    A class that defines a base model for other classes, providing methods for initializing instances, etc

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+"""Import modules datetime, uuid4 and storage"""
 from datetime import datetime
 from uuid import uuid4
 
@@ -5,7 +8,9 @@ from . import storage
 
 
 class BaseModel:
+	"""A base class for other models in the system"""
 	def __init__(self, *args, **kwargs):
+		"""instance of a new basemodel"""
 		if kwargs:
 			for key, value in kwargs.items():
 				if key == "__class__":
@@ -21,16 +26,20 @@ class BaseModel:
 			storage.new(self)
 
 	def __str__(self):
+		"""return a string representation of BaseModel instance"""
 		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
 	def __repr__(self):
+		"""returns an instance of a string representation"""
 		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
 	def save(self):
+		"""saves the instance"""
 		self.updated_at = datetime.today()
 		storage.save()
 
 	def to_dict(self):
+		"""converts the instnace to a dictionary"""
 		return self.__dict__ | {
 			"created_at": self.created_at.isoformat(),
 			"updated_at": self.updated_at.isoformat(),
@@ -39,22 +48,27 @@ class BaseModel:
 
 	@classmethod
 	def all(cls):
+		"""gets all instances of the class"""
 		return storage.get_all(cls.__name__)
 
 	@classmethod
 	def count(cls):
+		"""Gets the counts of the instance"""
 		return len(cls.all())
 
 	@classmethod
 	def show(cls, ids):
+		"""Shows the instance by its ID"""
 		return storage.get(f"{cls.__name__}.{ids}")
 
 	@classmethod
 	def destroy(cls, ids):
+		"""Destroys an instance by its ID"""
 		storage.delete(f"{cls.__name__}.{ids}")
 
 	@classmethod
 	def update(cls, ids, attr=None, value=None):
+		"""Updates the instance or attributes by its ID"""
 		obj = cls.show(ids)
 		if isinstance(attr, dict):
 			for key, value in attr.items():
